ceaabe7cbf720bbb1f6e36dfbf6f4c751790fd31
Ishank Gupta <ishankgupta94482i@gmail.com>
2023-09-24 02:22:49 +0530

    Added city repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
+const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async() => {
 
@@ -12,6 +13,8 @@ const setupAndStartServer = async() => {
 
     app.listen(PORT, () => {
         console.log(`server started at ${PORT}`);
+        const repo = new CityRepository();
+        repo.createCity({name:"New Delhi"});
     })
 
 }
