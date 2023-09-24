c8e54aa8e694af966ed52dcdfe68d048353ec8f8
Ishank Gupta <ishankgupta94482i@gmail.com>
2023-09-24 15:34:18 +0530

    added city service

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -45,6 +45,7 @@ class CityRepository{
     async getCity(cityId) {
         try {
             const city = await City.findByPk(cityId);
+            return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
             throw {error};
